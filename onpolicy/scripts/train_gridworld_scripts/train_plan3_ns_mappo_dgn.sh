6c32f90b08106b18ba17d0fda71afdd0252ef316
giangbang <banggiangle2015@gmail.com>
2024-11-04 11:23:04 +0700

    fix some configs

--- onpolicy/scripts/train_gridworld_scripts/train_plan3_ns_mappo_dgn.sh
+++ onpolicy/scripts/train_gridworld_scripts/train_plan3_ns_mappo_dgn.sh
@@ -11,7 +11,7 @@ do
     echo "seed is ${seed}:"
     CUDA_VISIBLE_DEVICES=0 python ../train/train_gridworld.py --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} \
     --plan ${plan} --seed ${seed} --hidden_size 64 --entropy_coef 0.01 --use_proper_time_limits --share_policy  \
-    --n_training_threads 1 --n_rollout_threads 1 --num_mini_batch 1 --episode_length 200 --num_env_steps 510_000 \
+    --n_training_threads 1 --n_rollout_threads 10 --num_mini_batch 1 --episode_length 200 --num_env_steps 510_000 \
     --ppo_epoch 5 --use_ReLU --gain 0.01 --lr 5e-4 --critic_lr 5e-4 --wandb_name "xxx" --user_name "_" --cuda --use_eval --deterministic_eval \
     --seperated_rewards --use_graph
 done
