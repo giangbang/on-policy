976a81244b5346ce8040ff79c6b4dc9af56d8425
giangbang <banggiangle2015@gmail.com>
2024-11-04 15:30:07 +0700

    add mpe configs files

--- onpolicy/scripts/train_mpe_scripts/train_mpe_spread_mgdapp_dgn.sh
+++ onpolicy/scripts/train_mpe_scripts/train_mpe_spread_mgdapp_dgn.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 env="MPE"
 scenario="simple_spread" 
-num_landmarks=5
-num_agents=5
+num_landmarks=3
+num_agents=3
 algo="mappo_mgdapp" #"mappo" "ippo"
 exp="check"
 seed_max=3
@@ -15,5 +15,5 @@ do
     --scenario_name ${scenario} --num_agents ${num_agents} --num_landmarks ${num_landmarks} --seed ${seed} \
     --n_training_threads 1 --n_rollout_threads 200 --num_mini_batch 1 --episode_length 25 --num_env_steps 5000000 \
     --ppo_epoch 10 --use_ReLU --use_eval --gain 0.01 --lr 3e-4 --critic_lr 7e-4 --wandb_name "xxx" --user_name "giangbang" \
-    --share_policy --cuda --deterministic_eval
+    --share_policy --cuda --deterministic_eval --use_graph
 done
